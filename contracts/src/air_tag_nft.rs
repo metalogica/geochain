f2e39022228d37a70dc5337dc9f7c8c20403ca2e
Richard Jarram <43706901+metalogica@users.noreply.github.com>
2024-04-20 19:34:53 -0400

    silence linter

--- contracts/src/air_tag_nft.rs
+++ contracts/src/air_tag_nft.rs
@@ -39,21 +39,21 @@ mod air_tag_nft {
             .globalize()
         }
 
-        pub fn mint(&mut self) -> Bucket {
-            let new_air_tag = AirTag {
-                name: "my new air tag",
-                description: "I'm ADHD so I love things all the time!",
-                serial: "SERIALOUSLY?!",
-            };
+        // pub fn mint(&mut self) -> Bucket {
+        //     let new_air_tag = AirTag {
+        //         name: "my new air tag",
+        //         description: "I'm ADHD so I love things all the time!",
+        //         serial: "SERIALOUSLY?!",
+        //     };
 
-            let new_air_tag_bucket = ResourceBuilder::new_ruid_non_fungible(OwnerRole::None)
-              .metadata(metadata! {
-                  init {
-                    "name" => "Real world asset - Air Tag", locked;
-                    "symbol" => "AIRTAG", locked;
-                  }
-                })
-        }
+        // let new_air_tag_bucket = ResourceBuilder::new_ruid_non_fungible(OwnerRole::None)
+        //   .metadata(metadata! {
+        //       init {
+        //         "name" => "Real world asset - Air Tag", locked;
+        //         "symbol" => "AIRTAG", locked;
+        //       }
+        //     })
+        // }
 
         pub fn take(&mut self) -> Bucket {
             info!(
