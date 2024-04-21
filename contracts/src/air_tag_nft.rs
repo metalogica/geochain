d960ad5c632a9d740974be395bef5257a1d65986
Richard Jarram <43706901+metalogica@users.noreply.github.com>
2024-04-20 20:32:40 -0400

    mint fn

--- contracts/src/air_tag_nft.rs
+++ contracts/src/air_tag_nft.rs
@@ -14,12 +14,13 @@ mod air_tag_nft {
         // gps_events: Vec<GpsEvent>,
         // vault: Vault,
         air_tag_resource_manager: ResourceManager,
+        total_supply: u64,
     }
 
     impl AirTagNft {
         pub fn instantiate_component() -> Global<AirTagNft> {
             let air_tag_resource_manager =
-                ResourceBuilder::new_ruid_non_fungible::<AirTag>(OwnerRole::None)
+                ResourceBuilder::new_integer_non_fungible::<AirTag>(OwnerRole::None)
                     .metadata(metadata! {
                       init {
                         "name" => "Real world asset - Air Tag", locked;
@@ -30,10 +31,28 @@ mod air_tag_nft {
 
             Self {
                 air_tag_resource_manager,
+                total_supply: 0,
             }
             .instantiate()
             .prepare_to_globalize(OwnerRole::None)
             .globalize()
+        }
+
+        pub fn mint(&mut self) -> Bucket {
+            let new_air_tag = AirTag {
+                name: "my new air tag".into(),
+                description: "I'm ADHD so I love things all the time!".into(),
+                serial: "SERIALOUSLY?!".into(),
+            };
+
+            let new_air_tag_bucket = self.air_tag_resource_manager.mint_non_fungible(
+                &NonFungibleLocalId::Integer(self.total_supply.into()),
+                new_air_tag,
+            );
+
+            self.total_supply += 1;
+
+            new_air_tag_bucket
         }
 
         // pub fn mint(&mut self) -> Bucket {
